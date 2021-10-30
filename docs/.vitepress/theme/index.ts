c8c4eefdc17583db2bdafc9abc9659138fc422f4
Lyoko <519052532@qq.com>
2021-10-30 11:54:30 +0800

    chore: deploy docs

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,8 @@
 // @ts-nocheck
 import { App } from "vue";
 import defaultTheme from "vitepress/dist/client/theme-default";
-import esriConfig from "@arcgis/core/config.js";
-esriConfig.assetsPath = "/vue-arcgis-api/assets";
+// import esriConfig from "@arcgis/core/config.js";
+// esriConfig.assetsPath = "/vue-arcgis-api/assets";
 import "@arcgis/core/assets/esri/themes/light/main.css";
 import components from "../components";
 
