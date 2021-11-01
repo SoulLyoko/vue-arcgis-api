67ac3a3e8e2ac37b8aab0268c23c2d45559c1046
Lyoko <519052532@qq.com>
2021-11-01 15:06:58 +0800

    chore: beta

--- packages/index.ts
+++ packages/index.ts
@@ -1,9 +1,10 @@
-import { App } from "vue";
+import { App, install } from "vue-demi";
 import * as components from "./components";
 export * from "./components";
 
 export default {
   install: (app: App): void => {
+    install();
     Object.values(components).forEach(component => {
       app.component(component.name, component);
     });
