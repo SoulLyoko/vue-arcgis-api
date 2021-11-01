31989080a01cd4e32625aecd2a7ea862bd1181f7
Lyoko <519052532@qq.com>
2021-11-01 19:37:22 +0800

    chore: main

--- packages/index.ts
+++ packages/index.ts
@@ -1,10 +1,9 @@
-import { App, install } from "vue-demi";
+import { App } from "vue-demi";
 import * as components from "./components";
 export * from "./components";
 
 export default {
   install: (app: App): void => {
-    install();
     Object.values(components).forEach(component => {
       app.component(component.name, component);
     });
--- packages/use/initView.ts
+++ packages/use/initView.ts
@@ -64,6 +64,7 @@ export function useInitView({
     h(
       "div",
       {
+        attrs: { id: containerId },
         id: containerId,
         style: "width:100%;height:100%"
       },
