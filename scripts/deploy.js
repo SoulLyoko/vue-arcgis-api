c2b04930c49ef519bd6ae928fa4c314242a59c84
Lyoko <519052532@qq.com>
2022-01-06 11:41:15 +0800

    ci: test

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,4 @@
-const execa = require("execa");
+const path = require("path");
+const { exec } = require("./utils");
 
-async function main() {
-  await execa("yarn", ["docs:build"]);
-  await execa("cd", ["docs/.vitepress/dist"]);
-  await execa("git", ["init"]);
-  await execa("git", ["add", "-A"]);
-  await execa("git", ["commit", "-m", "chore: deploy docs"]);
-  await execa("git", ["push", "-f", "https://github.com/SoulLyoko/vue-arcgis-api.git", "master:gh-pages"]);
-}
-
-main();
+exec(path.join("scripts", "deploy.sh"));
